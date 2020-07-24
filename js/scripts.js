e60a0847f5e472a725347ce926ea780811732d3a
John Karima <karimajohn24@gmail.com>
2020-07-24 16:47:49 +0300

    add js code for 'what we do' section

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+  $(".designDo").click(function () {
+    $(".designDes").fadeToggle();
+  });
+  $(".developmentDo").click(function () {
+    $(".developmentDes").fadeToggle();
+  });
+  $(".productDo").click(function () {
+    $(".productDes").fadeToggle();
+  });
+});
